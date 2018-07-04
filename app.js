32b7b6610063fa31885493dd97a759b96bd28415
SorayaSM <soraya.sacaan@gmail.com>
2018-07-04 09:03:05 -0400

    commit inicial

---
+++ app.js
@@ -0,0 +1,6 @@
+//Preguntar la temperatura
+const celsius = prompt("¿Cuál es la temperatura en Celsius?");
+//Convertir temperatura
+const convertirAFahrenheit = (celsius * 1.8) + 32;
+// Mostrar en consola
+console.log("La temperatura en Fahrenheit es " + convertirAFahrenheit);
